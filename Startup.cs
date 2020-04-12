416c8ac44172825f9e271d6c8e32b56329fb6d76
Marco Rabbolini <m.rabbo240@gmail.com>
2020-04-12 15:18:37 +0200

    Aggiornata l'appl a ASP.NET Core 2.2

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MyCourse
@@ -15,7 +16,8 @@ namespace MyCourse
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //services.AddMvc();  // versione 2.1
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -46,7 +48,7 @@ namespace MyCourse
             //app.UseMvcWithDefaultRoute();
             app.UseMvc(routeBuilder =>
             {
-                routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}")
+                routeBuilder.MapRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
